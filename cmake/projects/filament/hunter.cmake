edf2d5ade79f53896c3f70fbb74aebcb7a41111e
John V. Busch <john@johnvbusch.com>
2019-04-19 16:29:35 -0700

    updated filament hash and version

--- cmake/projects/filament/hunter.cmake
+++ cmake/projects/filament/hunter.cmake
@@ -9,11 +9,11 @@ hunter_add_version(
         PACKAGE_NAME
         filament
         VERSION
-        1.2.3
+        1.2.4
         URL
-        "https://github.com/JayBusch/filament/archive/v1.2.3.tar.gz"
+        "https://github.com/JayBusch/filament/archive/v1.2.4.tar.gz"
         SHA1
-        9780c909def7e31434ee443d372829163199edfe
+        38bd5663cd21445f411c386e1481f74fa34ec7db
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
